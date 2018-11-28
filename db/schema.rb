10cf7474d887d0e7bdf5ed30ef2fb7a988a3e607
Yazmin <yazdavast@gmail.com>
2018-11-28 11:32:01 -0300

    correccion

--- db/schema.rb
+++ db/schema.rb
@@ -68,10 +68,6 @@ ActiveRecord::Schema.define(version: 20181128131019) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "estado"
-    t.string "marcaTarj"
-    t.integer "numTarj"
-    t.date "fechaVencTarj"
     t.index ["email"], name: "index_clients_on_email", unique: true
     t.index ["reset_password_token"], name: "index_clients_on_reset_password_token", unique: true
   end
